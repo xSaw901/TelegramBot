b6083ec2997bdf8c0b87410641d6d24f62bee6d3
xSaw901 <57790393+xSaw901@users.noreply.github.com>
2022-12-11 19:33:22 +0300

    Final

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ def output(table):
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btnHelp = types.KeyboardButton("/help")
-    btnEstimate = types.KeyboardButton("/estimate")
+    btnEstimate = types.KeyboardButton("/how_estimate")
     btnRating = types.KeyboardButton("/rating")
     markup.add(btnHelp, btnEstimate, btnRating)
     bot.send_message(message.from_user.id, "Привет", reply_markup=markup)
@@ -46,22 +46,23 @@ def help(message):
     """
         /start-<b>Такая штука что бы начать и что бы кнопочки появились</b>
     /help-<b> Ну тут я надеюсь объяснений не надо</b>
-    /estimate-<b>Ну это надо что бы оценить плюшки всякие, тыкни, там инструкция как оценивать</b>
+    /how_estimate- <b>Показывает инструкцию для оценивания</b>
+    /estimate_it- <b>Позволяет оценить выпечку</b>
     /rating-<b>Просто рейтинг всех плюшек, всё ясно и понятно</b>
     """, 
     parse_mode='html')
 
 # обработчик команды /estimate, показывает инструкцию к оценке и кнопочку для вывода всего списка булочек
-@bot.message_handler(commands = ['estimate'])
+@bot.message_handler(commands = ['how_estimate'])
 def estimate(message):
     inline = types.InlineKeyboardMarkup()
     btnEstimate = types.InlineKeyboardButton(text = "Тык", callback_data = "Список")
     inline.add(btnEstimate)
     bot.send_message( message.from_user.id, 
     "Для оценки пирожочка надо будет написать \n" +
-    "/оценка <оценка>(1-5) <номер булочки>. \n" +
+    "/estimate_it <оценка>(1-5) <номер булочки>. \n" +
     "Например: \n" +
-    "            /оценка 5 6 \n" +
+    "            /estimate_it 5 6 \n" +
     "Если понял(а) то тыкай кнопку снизу там список будет",
     reply_markup = inline )
 #a = sql.execute("select id, Name from rating")
@@ -74,18 +75,31 @@ def inline(message):
         catalog = list(sql.execute("select id, Name from rating"))
         bot.send_message(message.from_user.id, '\n'.join(output(catalog)))
 
-# обработчик команды /оценка, позволяет поставить оценку определенному товару
-@bot.message_handler(commands = ['оценка'])
+# обработчик команды /estimate-it, позволяет поставить оценку определенному товару
+@bot.message_handler(commands = ['estimate_it'])
 def evaluation(message):
-        fSpace = str(message.text).find(' ')
-        print(fSpace)
-        if fSpace != -1:
-            param = str(message.text[fSpace+1:]).split(" ")
+        firstSpace = str(message.text).find(' ')
+        print(firstSpace)
+        if firstSpace != -1:
+            param = str(message.text[firstSpace+1:]).split(" ")
+            minMaxBakery = list(sql.execute("select min(id), max(id) FROM rating"))
+            print (minMaxBakery)
+            minBakeryNumber = minMaxBakery[0][0]
+            maxBakeryNumber = minMaxBakery[0][1]
+
+            if len(param) != 2:
+                bot.send_message(message.from_user.id, "Необходимо указать 2 значения: оценка и номер выпечки")
+                return
             rate = param[0]
             bakeryNumber = param[1]
-            lenTable = list(sql.execute("select id from rating"))
-            if  len(param) != 2 or int(rate) > 5 or int(rate) <= 0 or int(bakeryNumber) > len(lenTable):
-                bot.send_message(message.from_user.id, "Поставте оценку от 1 до 5. Или проверьте имеется ли булочка с таким номером.")
+            if rate.isdigit() != True or bakeryNumber.isdigit() != True:
+                bot.send_message(message.from_user.id, "Оценка и/или номер выпечки должны быть числами")
+                return
+            if  int(rate) > 5 or int(rate) <= 0:
+                bot.send_message(message.from_user.id, "Поставте оценку от 1 до 5.")
+                return
+            if  int(bakeryNumber) < minBakeryNumber or  int(bakeryNumber) > maxBakeryNumber:
+                bot.send_message(message.from_user.id, "Проверьте имеется ли булочка с таким номером.")
             else:
                 sql.execute(f"update rating set Count=Count+1, Summ=Summ+{ round(float(rate),1) } where id ={ int(bakeryNumber) }")
                 db.commit()
@@ -94,9 +108,9 @@ def evaluation(message):
             bot.send_message(message.from_user.id, 
             """
             Необходимо вводить:
-            </оценка <оценка булочки> <номер булочки>
+            </estimate_it <оценка булочки> <номер булочки>
             Например:
-            /оценка 5 6
+            /estimate_it 5 6
              """)
 
 # обработчик комманды /rating, выводит список булочек с их рейтингом
